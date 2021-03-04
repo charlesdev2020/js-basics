9810cabda7ceaf71f762750fc975d2f2cfeff0b8
charlesdev2020 <75482355+charlesdev2020@users.noreply.github.com>
2021-03-04 19:49:21 +0900

    #3.8 Getting the Weather part One Geolocation

--- bg.js
+++ bg.js
@@ -1,6 +1,6 @@
 const body = document.querySelector("body");
 
-const IMG_NUMBER = 7;
+const IMG_NUMBER = 5;
 
 // when load images from API
 function handleImgLoad() {
---
+++ weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "OPEN_WEATHER_MAP_USER_API_KEY";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("Can not access geo location.");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    // getWeather
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
